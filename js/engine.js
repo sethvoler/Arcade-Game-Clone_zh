f167e55280a00e2f31743799130bff60d9780f3f
周琰 <zhouyan@zhouyandeMacBook-Pro.local>
2017-06-02 19:21:55 +0800

    增加了表示血量的图片节点

--- js/engine.js
+++ js/engine.js
@@ -21,6 +21,16 @@ var Engine = (function(global) {
         ctx = canvas.getContext('2d'),
         lastTime;
 
+    var blood = doc.getElementById("blood");
+
+    for(var i = 0; i < 10; i++) {
+        var img = document.createElement("img");
+        img.id="img" + i;
+        img.setAttribute('src','images/blood.png');
+        blood.appendChild(img);
+    }
+
+
     canvas.width = 505;
     canvas.height = 606;
     doc.body.appendChild(canvas);
@@ -144,7 +154,8 @@ var Engine = (function(global) {
         'images/char-cat-girl.png',
         'images/char-horn-girl.png',
         'images/char-pink-girl.png',
-        'images/char-princess-girl.png'
+        'images/char-princess-girl.png',
+        'images/blood.png'
     ]);
     Resources.onReady(init);
 
